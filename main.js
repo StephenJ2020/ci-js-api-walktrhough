56caed41c035e68bb026bbadf5a5cc24b7c31f32
Stephen <sjohnston.ie@gmail.com>
2021-04-26 17:44:09 +0000

    maint: Make our code to wait on execution

--- main.js
+++ main.js
@@ -4,16 +4,18 @@ var data;
 xhr.open('GET', 'https://ci-swapi.herokuapp.com/api/');
 xhr.send();
 
-function setData(jsonData){
-    data = jsonData;
+// function setData(jsonData){
+    // data = jsonData;
     // console.log(data);  
     // this doesn't solve the problem, now all the code for the site needs to be enclosed in this "setData" function instead of the "onreadystatechange" function
-}
+// } 
+// In video 5 after we add 'setTimeout()' the tutor then instructs to remove the 'setData' function and revert back to the original code we had in the 'xhr function'
 
 xhr.onreadystatechange = function(){
-    console.log(this.readyState);
+    // console.log(this.readyState);  Now we remove this line of code as well
     if (this.readyState == 4 && this.status == 200){
-       setData(JSON.parse(this.responseText));
+       // setData(JSON.parse(this.responseText));  Now we remove this again and amend it to the code below
+       data = JSON.parse(this.responseText);
     }
 };
 
