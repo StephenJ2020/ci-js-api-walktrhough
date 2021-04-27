b36f9c5ccd24a8ae4104e0aca88a9dfc6c1ce1fd
Stephen <sjohnston.ie@gmail.com>
2021-04-27 18:01:16 +0000

    feat: Add forLoop to create tableHeaders

--- main.js
+++ main.js
@@ -13,30 +13,89 @@ function getData(type, cb){   // cb stands for 'call back'
     };
 }
 
-    /*    function writeToDocument(type){
-            getData(type, function(data){
-                // console.dir(data);  // Print the contents of the object to a directory within the console window    
-                data = data.results;
+function getTableHeaders(obj){
+    var tableHeaders = [];
 
-                data.forEach(function(item){  // A For Loop
-                    // document.getElementById('data').innerHTML = item.name;  // Now that we have this unpacked into JSON format, we can just do "item.name".
-                                                                            // = will over-write the previous data, I need to do a "+=".
-                    document.getElementById('data').innerHTML += '<p>' + item.name + '</p>';  // But this list contents of each button in one long list
-                })
-                
-            });
-        }
-    */
+    Object.keys(obj).forEach(function(key){
+        tableHeaders.push(`<td>${key}</td>`);  // template literal with & thingy to read 'key value
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
+}
 
 function writeToDocument(type){
     var el = document.getElementById('data');  // el short for element but cant use element as a variable name as it is a reserved word.
     el.innerHTML = '';
+
     getData(type, function(data){  
         data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
 
         data.forEach(function(item){  // A For Loop
-            el.innerHTML += '<p>' + item.name + '</p>';
-        })
+            //el.innerHTML += '<p>' + item.name + '</p>';
+        });
         
+        el.innerHTML = `<table>${tableHeaders}</table>`;
+
     });
 }
+
+/*
+As well as getting our films button working, what we'd really like to do is be able to display the data nicely and neatly in a table format.
+And that's what we're going to start doing in this video.
+Now in order to do this, JavaScript allows us to iterate over all of the keys.
+Remember that the data is stored in key-value pairs; keys such as "name", and values such as Luke Skywalker, Obi-Wan Kenobi, so on and so forth.
+So if I do "object.keys", and then I pass in "item" here inside our forEach loop.
+And do another forEach loop inside.
+Then I can actually iterate over each of these keys.
+And I'm going to put a function inside here again, just like we have with our first forEach loop.
+And I'm just going to "console.log" each key.
+*/
+
+/*
+Okay, so if I save that, go over to my browser, click on films, then you'll see that it gives me a list of all of the keys.
+And we can see that the problem is films doesn't have a key called "name".
+It has "title" instead, which becomes a problem because we're dealing specifically with a name.
+But what we want to do is use this kind of approach to iterate over the keys to build ourselves a table full of data without actually explicitly specifying a property.
+And we can allow JavaScript to do that for us.  
+*/
+
+/*
+So we're going to create a new function called "getTableHeaders".
+And this is going to take in a single object, we'll just call it "obj".
+And then we're going to create a new array called "tableHeaders", which we're going to initialize as an empty array.
+After that, we're going to use the same code again.
+We're going to take our object and iterate over the keys.
+So we'll do a forEach function once again.
+The function inside this is going to iterate over each key and push it to our tableHeaders array.
+*/
+
+/*
+Now, we're not just going to push the key.
+We want this to be formatted nicely, so we're going to send in a <td> to create a new table cell.
+Then the key.
+And then we're going to close our table cell.
+And what we're going to do is add each of those to a row.
+Notice I'm using this backtick, or back quote, formation here. Those are not standard single quotes.
+This is something called a template literal, which allows us to interpolate variables and strings like this.
+Okay, so I'm going to return the tableHeaders.
+Back inside our writeToDocument function, we now need to invoke that function where we're calling our getTableHeaders.
+*/
+
+/* 
+So once we've retrieved our data, we'll call the getTableHeaders function.
+Now we're going to pass through the first object in the array.
+And just for now, we're going to go down to the end and set the innerHTML of "el" to our table headers.
+So I'll just comment out the other one and just set the innerHTML.
+And we're going to use the template literal again to send in a table.
+And then we're going to write the tableHeaders variable in there.
+No, I actually haven't created the tableHeaders variable yet on line 32, so I'll go back and correct that in a second.
+Close the table.
+Semicolon
+And I'm going to call this "tableHeaders".
+"var tableHeaders = getTableHeaders(data[0])"
+So now if I refresh the page and click on films, we can see I have a table row that contains each of the keys from a film object.
+So that's good.
+We don't have to specify name or any of the properties that are contained in our data. It's going to build the table for us.
+In our next video, we're going to add to this table and get some more data displaying on screen.
+*/
