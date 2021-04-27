3bbc7ef5f175806f7ced1e6e57ebfe39280d2995
Stephen <sjohnston.ie@gmail.com>
2021-04-27 12:43:52 +0000

    feat: Add buttons and create const baseURL

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-function getData(cb){   // cb stands for 'call back'
+const baseURL = 'https://ci-swapi.herokuapp.com/api/';
+
+function getData(type, cb){   // cb stands for 'call back'
     var xhr = new XMLHttpRequest();
 
-    xhr.open('GET', 'https://ci-swapi.herokuapp.com/api/');
+    xhr.open('GET', baseURL + type + '/');
     xhr.send();
 
     xhr.onreadystatechange = function(){
@@ -11,25 +13,8 @@ function getData(cb){   // cb stands for 'call back'
     };
 }
 
-// Function
-function printDataToConsole(data){
-    console.log(data);
+function writeToDocument(type){
+    getData(type, function(data){
+        document.getElementById('data').innerHTML = data;
+    });
 }
-
-//  // Argument
-//  getData(function(data){
-//      console.log(data);
-//  });
-
-getData(printDataToConsole);
-
-// So as you can see, callback functions are very useful.
-// They get around the problem of having to use timeouts.
-// They still allow us full control over our data because they're only invoked when we actually want them to be.
-
-// So in this unit, we've looked at how to speak to our API using JavaScript.
-// We've seen how to get our response text back as a string and how to turn it into JSON format.
-// We've looked at different readyStates and HTTP status codes.
-// And finally, we've seen how to use both timeouts and callback functions to get our data displaying when we want it to.
-// Now that we know how to get the data and how to parse it, let's start doing something useful with it.
-// We'll have a look at that in our next unit.
